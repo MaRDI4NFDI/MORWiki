ff4ca196deb3237ed460711ea40de9a132f51a31
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2025-05-29 19:03:47 +0200

    add missing object imports in __init__

--- src/morwiki/__init__.py
+++ src/morwiki/__init__.py
@@ -4,5 +4,5 @@ Copyright (c) 2025 MORB Developers and Contributors. All rights reserved.
 morwiki: MORWiki Fetcher for MORB
 """
 
-from morwiki.config import Settings, print_config, get_config
-from morwiki.examples import Database, Example, fetch_example_meta
+from morwiki.config import Settings, print_config, get_config, clear_config
+from morwiki.examples import Database, Example, fetch_example_meta, get_database
