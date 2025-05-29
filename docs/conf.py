3b4750738f093bcfde740ba1e71b547d7d1be904
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2025-05-29 09:50:04 +0200

    Add docs and improve README

---
+++ docs/conf.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import importlib.metadata
+from typing import Any
+
+project = "morwiki"
+copyright = "2025, Ashwin Nayak"
+author = "Ashwin Nayak"
+version = release = importlib.metadata.version("morwiki")
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+]
+
+source_suffix = [".rst", ".md"]
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
+
+html_theme = "furo"
+
+html_theme_options: dict[str, Any] = {
+    "source_repository": "https://gitlab.mpi-magdeburg.de/mardi/morwiki",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+
+myst_enable_extensions = [
+    "colon_fence",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+nitpick_ignore = [
+    ("py:class", "_io.StringIO"),
+    ("py:class", "_io.BytesIO"),
+]
+
+always_document_param_types = True
