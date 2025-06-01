bf7c29f2659abf5e2ea4ea0bff72d961698d8f3a
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2025-06-01 19:25:49 +0200

    make pkg api cleaner

--- src/morwiki/__init__.py
+++ src/morwiki/__init__.py
@@ -6,3 +6,14 @@ morwiki: MORWiki Fetcher for MORB
 
 from morwiki.config import Settings, print_config, get_config, clear_config
 from morwiki.examples import Database, Example, fetch_example_meta, get_database
+
+__all__ = [
+    "Settings",
+    "get_config",
+    "clear_config",
+    "print_config",
+    "Database",
+    "Example",
+    "fetch_example_meta",
+    "get_database",
+]
