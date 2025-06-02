5613eb75941dad2537bcf7a2a9ba94b8dc4c93d6
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2025-06-02 23:04:16 +0200

    fix minor bug in main

--- src/morwiki/__main__.py
+++ src/morwiki/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from platformdirs import user_config_dir
+from platformdirs import user_config_dir, user_cache_dir
 from rich import print
 
 parser = argparse.ArgumentParser(
@@ -34,15 +34,15 @@ if args.create_config is not None:
     else:
         config_dir = Path(args.create_config).expanduser().resolve()
 
-    config_data = """# Server configuration for MORWiki
-    # Server URL, database file and file hash
-    serverurl: "https://csc.mpi-magdeburg.mpg.de/mpcsc/MORB-data/"
-    indexfile: "examples.csv"
-    indexfilehash: "sha256:960a243420e3e2d229bebb26313541841c6d5b51b9f215d7ca7b77c6b3636791"
-
-    # Custom Cache location : (by default it is the OS-specific user cache directory)
-    # cache_dir: "~/.cache/morwiki"
-    """
+    config_data = (
+        "# Server configuration for MORWiki\n"
+        "# Server URL, database file and file hash\n"
+        "serverurl: \"https://csc.mpi-magdeburg.mpg.de/mpcsc/MORB-data/\"\n"
+        "indexfile: \"examples.csv\"\n"
+        "indexfilehash: \"sha256:960a243420e3e2d229bebb26313541841c6d5b51b9f215d7ca7b77c6b3636791\"\n"
+        "# Custom Cache location\n"
+        f"cache_dir: \"{user_cache_dir(appname='morwiki', appauthor='morb-users')}\"\n"
+    )
 
     yaml_path = config_dir / "morwiki.config.yaml"
     with open(yaml_path, "w") as f:
