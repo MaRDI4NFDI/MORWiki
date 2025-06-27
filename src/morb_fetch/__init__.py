66229463b0cde7b614172aa7c9530d7a3fca7a22
Ashwin Nayak <anayak@mpi-magdeburg.mpg.de>
2025-06-27 22:07:13 +0200

    add mmess downloaders; rename morlab.py>toolkits.py

--- src/morb_fetch/__init__.py
+++ src/morb_fetch/__init__.py
@@ -6,7 +6,7 @@ morb-fetch: Data Fetcher for MORB
 
 from morb_fetch.config import Settings, print_config, get_config, clear_config
 from morb_fetch.examples import Database, Example, get_database
-from morb_fetch.morlab import MORLABDownloader
+from morb_fetch.toolkits import ToolkitDownloader, MORLABDownloader, MMESSDownloader
 
 __all__ = [
     "Settings",
@@ -15,6 +15,8 @@ __all__ = [
     "print_config",
     "Database",
     "Example",
+    "ToolkitDownloader",
     "MORLABDownloader",
+    "MMESSDownloader",
     "get_database",
 ]
--- src/morb_fetch/toolkits.py
+++ src/morb_fetch/toolkits.py
@@ -1,7 +1,7 @@
 import pooch
-from typing import Annotated, Optional
+from typing import Annotated, ClassVar, Optional
 from typing_extensions import Doc
-from pydantic import StringConstraints
+from pydantic import BaseModel, StringConstraints
 
 from morb_fetch.config import get_config, Settings
 
@@ -18,29 +18,24 @@ DOIstr = Annotated[
 """ DOIstr: A string starting with 'doi:' followed by record id. """
 
 
-class MORLABDownloader:
+class ToolkitDownloader(BaseModel):
     """
     A class to download MORLAB releases from Zenodo
     """
-
-    _registry: dict[str, DOIstr] = {
-        "6.0": "doi:10.5281/zenodo.7072831",
-        "5.0": "doi:10.5281/zenodo.3332716",
-        "4.0": "doi:10.5281/zenodo.1574083",
-        "3.0": "doi:10.5281/zenodo.842659",
-    }
+    name: ClassVar[str]
+    registry: ClassVar[dict[str, DOIstr]]
 
     @classmethod
     def list_available_versions(cls) -> list[str]:
         """
         List all available versions of MORLAB
         """
-        return list(cls._registry.keys())
+        return list(cls.registry.keys())
 
     @classmethod
     def retrieve_version(cls, version: str, config: Optional[Settings]=None) -> str:
         """
-        Retrieve a specific version of MORLAB
+        Retrieve a specific version of Toolkit
         """
         # assert that version is valid
         available_versions = cls.list_available_versions()
@@ -48,14 +43,14 @@ class MORLABDownloader:
             raise ValueError(f"Version {version} not found. Available versions: {available_versions}")
 
         # metadata
-        doi = cls._registry[version]
-        zip_filename = f"morlab-{version}.zip"
+        doi = cls.registry[version]
+        zip_filename = f"{cls.name}-{version}.zip"
         config = get_config()
 
         # build downloader from doi
         downloader = pooch.create(
             base_url=doi,
-            path=config.cache / "morlab",
+            path=config.cache / cls.name,
             registry=None,
         )
         downloader.load_registry_from_doi()
@@ -67,7 +62,30 @@ class MORLABDownloader:
             progressbar=True,
         )
 
-        unzip_path = config.cache / "morlab" / f"morlab-{version}"
-        print(f"MORLAB-{version} downloaded at {unzip_path}")
+        unzip_path = config.cache / cls.name / f"{cls.name}-{version}"
+        print(f"{cls.name}-{version} downloaded at {unzip_path}")
 
         return str(unzip_path)
+
+
+class MORLABDownloader(ToolkitDownloader):
+    name = "morlab"
+    registry = {
+        "6.0": "doi:10.5281/zenodo.7072831",
+        "5.0": "doi:10.5281/zenodo.3332716",
+        "4.0": "doi:10.5281/zenodo.1574083",
+        "3.0": "doi:10.5281/zenodo.842659",
+    }
+
+
+class MMESSDownloader(ToolkitDownloader):
+    name = "MMESS"
+    registry = {
+        "3.1": "doi:10.5281/zenodo.14929081",
+        "3.0": "doi:10.5281/zenodo.7701424",
+        "2.2": "doi:10.5281/zenodo.5938237",
+        "2.1": "doi:10.5281/zenodo.4719688",
+        "2.0.1": "doi:10.5281/zenodo.3606345",
+        "2.0": "doi:10.5281/zenodo.3368844",
+        "1.0.1": "doi:10.5281/zenodo.50575",
+    }
